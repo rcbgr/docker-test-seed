e7146aeb0a1bd35db190043ee819c9605220302e
Ryan <ryannitz@amazon.com>
2019-02-25 17:42:26 -0500

    Fix push cmd

--- push.sh
+++ push.sh
@@ -8,4 +8,6 @@ docker build -t blog-post-seed .
 
 docker tag blog-post-seed:latest $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/blog-post-seed:latest
 
-docker tag blog-post-seed:latest $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/blog-post-seed:latest
+docker push $AWS_ACCOUNT_ID.dkr.ecr.us-east-1.amazonaws.com/blog-post-seed:latest
+
+
