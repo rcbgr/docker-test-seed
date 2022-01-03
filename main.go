152204a053b6097d2e207d9fdc6c07cc582a4ed0
Ryan Nitz <ryannitz@ip-10-59-1-138.ec2.internal>
2022-01-03 17:46:25 -0500

    Use a cert and add health path

--- main.go
+++ main.go
@@ -5,30 +5,23 @@ import (
 	"io"
 	"net/http"
 	"os"
-	"strconv"
-	"strings"
+	"log"
 )
 
 func main() {
 
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-
-		f := fib()
-
 		w.WriteHeader(http.StatusOK)
 		io.WriteString(w, "Hello World\n")
-
-		for _, e := range os.Environ() {
-			pair := strings.Split(e, "=")
-			io.WriteString(w, pair[0]+"="+pair[1]+"\n")
-		}
-
-		for i := 1; i <= 90; i++ {
-			io.WriteString(w, strconv.Itoa(f())+"\n")
-		}
 	})
 
-	port := "8080"
+	http.HandleFunc("/health", func(w http.ResponseWriter, r *http.Request) {
+		w.WriteHeader(http.StatusOK)
+		io.WriteString(w, "ok\n")
+	})
+
+
+	port := "8443"
 
 	if len(os.Getenv("PORT")) > 0 {
 		port = os.Getenv("PORT")
@@ -36,15 +29,7 @@ func main() {
 
 	fmt.Println(fmt.Sprintf("starting listener on: %s", port))
 
-	if err := http.ListenAndServe(fmt.Sprintf(":%s", port), nil); err != nil {
-		fmt.Println(err)
-	}
-}
-
-func fib() func() int {
-	a, b := 0, 1
-	return func() int {
-		a, b = b, a+b
-		return a
+	if err := http.ListenAndServeTLS(fmt.Sprintf(":%s", port), "server.crt", "server.key", nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
 	}
 }
